0d317bd3fd93bdf428a67744a784849abcc3d091
JVR-07 <machado.jvr07@gmail.com>
2026-03-16 18:26:41 -0700

    refactor(prisma): configure Pg adapter for connection pooling

--- coverly-core/lib/prisma.ts
+++ coverly-core/lib/prisma.ts
@@ -1,7 +1,17 @@
 import { PrismaClient } from "../app/generated/prisma";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const getConnection = () => {
+  if (globalForPrisma.prisma) return globalForPrisma.prisma;
+  
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool as any);
+  return new PrismaClient({ adapter });
+};
+
+export const prisma = getConnection();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
